afddc69c114749a782cdd7d024cdbece74f07bfc
blakereeves19 <blake.reeves19@gmail.com>
2019-02-01 13:46:00 -0600

    Updated delete button

--- src/client/Timeline.tsx
+++ src/client/Timeline.tsx
@@ -19,10 +19,7 @@ const Timeline: React.SFC<WelcomeProps> = (props) => {
     return (
         <Col sm="12">
             <Card body className="col-md-8 mx-auto m-2">
-                <Button id="deleteChirp">X</Button>
-                {$('#deleteChirp').click(() => {
-                    deleteChirp(props.chirp.id);
-                })}
+                <Button onClick={() => deleteChirp(props.chirp.id)} type="submit" id="deleteChirp">X</Button>
                 <CardTitle className="p-2 border border-dark border-top-0 border-left-0 border-right-0">{props.chirp.user} Chirped!</CardTitle>
                 <CardText className="p-2">{props.chirp.text}</CardText>
                 <CardFooter className="mt-2">{props.chirp.id}</CardFooter>
